0c601be5dfe71a2176d898a3a4162815f55a5a83
khanzadalo <khanzadaomorova@gmail.com>
2024-02-16 19:21:31 +0600

    hw5

--- tv_show/urls.py
+++ tv_show/urls.py
@@ -3,13 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.tv_shows_list, name='tv_shows_list'),
-    path('tv_shows_list/<int:id>/', views.tv_show_detail, name='tv_show_detail'),
-    path('tv_shows_list/<int:id>/delete', views.tv_show_delete_view, name='delete_tvshow'),
-    path('tv_shows_list/<int:id>/update', views.tv_show_edit_view, name='update_tvshow'),
-    path('create_tvshow/', views.tv_show_create_view, name='create_tvshow'),
+    path('', views.TVShowListView.as_view(), name='tv_shows_list'),
+    path('tv_shows_list/<int:id>/', views.TVShowDetailView.as_view(), name='tv_show_detail'),
+    path('tv_shows_list/<int:id>/delete', views.DeleteTVShowView.as_view(), name='delete_tvshow'),
+    path('tv_shows_list/<int:id>/update', views.EditTVShowView.as_view(), name='update_tvshow'),
+    path('create_tvshow/', views.TVShowCreateView.as_view(), name='create_tvshow'),
     path('add_review/', views.add_review_view, name='add_review'),
-
-
+    path('search/', views.SearchTVShowView.as_view(), name='search'),
 
 ]
--- tv_show/views.py
+++ tv_show/views.py
@@ -1,52 +1,62 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from . import models, forms
+from django.views import generic
+
+class TVShowListView(generic.ListView):
+    model = models.TVShow
+    template_name = 'tv_shows/tv_shows_list.html'
+    context_object_name = 'tvshows'
+
+    def get_queryset(self):
+        tv_shows = self.model.objects.all()
+        for tv_show in tv_shows:
+            tv_show.review_count = tv_show.tvshow_reviews.count()
+        return tv_shows
 
 
-def tv_shows_list(request):
-    if request.method == 'GET':
-        tvshows = models.TVShow.objects.all()
-        return render(request, 'tv_shows/tv_shows_list.html',
-                      {'tvshows':tvshows})
+class TVShowDetailView(generic.DetailView):
+    template_name = 'tv_shows/tv_show_detail.html'
+    context_object_name = 'tvshow_id'
+
+    def get_object(self, **kwargs):
+        tvshow_id = self.kwargs.get('id')
+        return get_object_or_404(models.TVShow, id=tvshow_id)
 
 
-def tv_show_detail(request, id):
-    if request.method == 'GET':
-        tvshow_id = get_object_or_404(models.TVShow, id=id)
-        return render(request, 'tv_shows/tv_show_detail.html',
-                      {'tvshow_id': tvshow_id})
+class TVShowCreateView(generic.CreateView):
+    template_name = 'tv_shows/crud/create_tvshow.html'
+    form_class = forms.TVShowForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(TVShowCreateView, self).form_valid(form=form)
 
 
-# Add
-def tv_show_create_view(request):
-    if request.method == 'POST':
-        form = forms.TVShowForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Successfully added <a href="/">Main page</a> ')
-    else:
-        form = forms.TVShowForm()
-    return render(request, 'tv_shows/crud/create_tvshow.html',
-                  {'form': form})
+class DeleteTVShowView(generic.DeleteView):
+    template_name = 'tv_shows/crud/confirm_delete.html'
+    success_url = '/'
+
+    def get_object(self, **kwargs):
+        tvshow_id = self.kwargs.get('id')
+        return get_object_or_404(models.TVShow, id=tvshow_id)
 
 
-def tv_show_delete_view(request, id):
-    tvshow_id = get_object_or_404(models.TVShow, id=id)
-    tvshow_id.delete()
-    return HttpResponse('Successfully deleted <a href="/">Main page</a>')
+class EditTVShowView(generic.UpdateView):
+    template_name = 'tv_shows/crud/edit_tvshow.html'
+    form_class = forms.TVShowForm
+    success_url = '/'
+    queryset = models.TVShow.objects.all()
 
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(EditTVShowView, self).form_valid(form=form)
 
-def tv_show_edit_view(request, id):
-    tvshow_id = get_object_or_404(models.TVShow, id=id)
-    if request.method == 'POST':
-        form = forms.TVShowForm(instance=tvshow_id, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Successfully changed <a href="/">Main page</a>')
-    else:
-        form = forms.TVShowForm(instance=tvshow_id)
-        return render(request, 'tv_shows/crud/edit_tvshow.html',
-                  {'form': form, 'tvshow_id': tvshow_id})
+    def get_object(self, **kwargs):
+        tvshow_id = self.kwargs.get('id')
+        return get_object_or_404(models.TVShow, id=tvshow_id)
 
 
 def add_review_view(request):
@@ -58,3 +68,17 @@ def add_review_view(request):
     else:
         form = forms.ReviewForm()
         return render(request, 'tv_shows/add_review.html', {'form': form})
+
+
+class SearchTVShowView(generic.ListView):
+    template_name = 'tv_shows/tv_shows_list.html'
+    context_object_name = 'tvshows'
+    paginate_by = '5'
+
+    def get_queryset(self):
+        return models.TVShow.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
